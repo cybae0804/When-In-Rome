1d6f4a98e03400a7180fd61c7c088e0b11ff47e3
Cy Bae <cybae0804@gmail.com>
2018-12-27 20:46:14 -0800

    Search Component HTML Done

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import Search from './search';
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+        <Search/>
     </div>
 );
 
--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import '../assets/css/search.css';
 
 class Search extends Component {
-  
-  render() {
-    return (
-      <div>
-
+  landing = (
+    <form className="ui form container">
+      <div className="field">
+        <label>City or Job</label>
+        <input type="text" name="cityjob" placeholder="Tokyo, Japan"/>
       </div>
-    )
+      <div className='two fields'>
+        <div className="field small">
+          <label>Dates</label>
+          <input type="text" name="date" placeholder="mm/dd/yyyy"/>
+        </div>
+        <div className="field small">
+          <label>Guests</label>
+          <input type="text" name="guests" placeholder="1 guest"/>
+        </div>
+      </div>
+      <button className="fluid ui button positive" type="submit">Search</button>
+    </form>
+  )
+
+  default = (
+    <div className="ui fluid left icon input container">
+      <i className="search link icon"/>
+      <input type="text" placeholder="Osaka, Japan"/>
+    </div>
+  )
+
+  filter = (
+    <div className='ui container'>
+      <div className="ui fluid left icon input">
+        <i className="search link icon"/>
+        <input type="text" placeholder="Osaka, Japan"/>
+      </div>
+      <div>
+        <button className="ui inverted green button filterButtn">Filter</button>
+        <button className="ui inverted green button sortButton">Sort by Date</button>
+      </div>
+    </div>
+  )
+
+  render() {
+    return this.filter;
   }
 }
 
