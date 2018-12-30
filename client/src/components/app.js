fbc48cda329d6534bf83719f54f21ad817162da6
Hapachino <erickwbrownfield@gmail.com>
2018-12-29 18:05:13 -0800

    added axios test and changed App to class component

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import axios from 'axios';
 import ExperiencePreviewContainer from './experience_preview_container';
 import ExperiencePreview from './experience_preview';
 import images from './dummyImages';
@@ -25,16 +26,37 @@ const experiences = [
     averageRating: 4.2,
     totalRatings: 25,
   },
-]
+];
 
-const App = () => (
-  <div>
-    <ExperiencePreviewContainer
-      heading="explore"
-      experiences={experiences}
-    />
-    <Route path={'experience/:' } component={ExperiencePreview}/>
-  </div>
-);
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    axios.get('http://localhost:9000/api/experiences')
+      .then(res => {
+        console.log(res);
+    }).catch(err => {
+      console.log(err);
+    });
+
+    return (
+      <div>
+        <ExperiencePreviewContainer
+          heading="explore"
+          experiences={experiences}
+        />
+        <Route path={'experience/:'} component={ExperiencePreview} />
+      </div>
+    );
+  }
+}
 
 export default App;
