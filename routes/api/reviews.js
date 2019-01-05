ab0d5c61145711b4888725a0ce2d0012f335b19c
Cy Bae <cybae0804@gmail.com>
2019-01-05 13:07:35 -0800

    Waiting on dates column confirmation

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -2,5 +2,6 @@ const router = require('express').Router({ mergeParams: true });
 const { reviews } = require('../../controllers');
 
 // router.get('/', reviews.post);
+router.post('/', reviews.postOne);
 
 module.exports = router;
