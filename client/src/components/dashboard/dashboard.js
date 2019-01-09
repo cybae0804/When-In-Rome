a836ef20f17e2880ca36570c898fa32c591aeceb
Cy Bae <cybae0804@gmail.com>
2019-01-09 15:59:18 -0800

    Search field polished and added header option

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,17 +1,18 @@
 import React from 'react';
+
+// Components
+import Upcoming from './upcoming';
+import Reservations from './reservations';
+import ActiveListing from './active-listing';
+import History from './history';
+import Header from '../header/header';
+
 import './dashboard.css'
-import HamburgerButton from '../hamburger_button/hamburger_button'
-import Upcoming from './upcoming'
-import Reservations from './reservations'
-import ActiveListing from './active-listing'
-import History from './history'
 
 export default props => {
-  console.log(props)
   return(
     <div>
-      <h1 className="ui header container topMargin">Dashboard</h1>
-      <HamburgerButton/>
+      <Header noSearch title='Dashboard'/>
       <div className="ui equal width grid container topMargin">
         <button className="ui column button"><h3>As Host</h3></button>
         <button className="ui column button"><h3>As User</h3></button>
@@ -21,5 +22,5 @@ export default props => {
       <ActiveListing/>
       <History/>
     </div>
-  )
+  );
 }
