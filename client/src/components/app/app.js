0de4aa1391dc86bf31b2ed2c1d18d3a5b869c081
Cy Bae <cybae0804@gmail.com>
2019-01-12 23:01:35 -0800

    Rough implementation of filter dropdown

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -6,7 +6,6 @@ import { Route } from 'react-router-dom';
 import LandingPage from '../landing_page/landing_page';
 import SearchPage from '../search_page/search_page';
 import ExperiencePage from '../experience_page/experience_page';
-import FilterPage from '../filter_page/filter_page';
 import DashboardPage from '../dashboard_page/dashboard_page';
 
 // etc
@@ -23,7 +22,6 @@ class App extends Component {
         <Route exact path={'/'} component={LandingPage} />
         <Route path={'/search'} component={SearchPage} />
         <Route path={'/experience/:experience_id'} component={ExperiencePage} />
-        <Route path={'/filter/'} component={FilterPage} />
         <Route path={'/dashboard/'} component={DashboardPage}/>
       </div>
     );
