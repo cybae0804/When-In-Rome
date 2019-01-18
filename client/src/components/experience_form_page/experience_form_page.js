1aab5a04250b01d77e3ac21d00d7f6df23781306
Hapachino <erickwbrownfield@gmail.com>
2019-01-17 19:48:56 -0800

    added edit experience form with initival values from async call

--- client/src/components/experience_form_page/experience_form_page.js
+++ client/src/components/experience_form_page/experience_form_page.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import Header from '../shared/header/header';
 import Footer from '../shared/footer/footer';
 import ExperienceForm from '../shared/experience_form/experience_form';
-import './create_experience_page.css';
+import './experience_form_page.css';
 
-class CreateExperiencePage extends Component {
+class ExperienceFormPage extends Component {
   render() {
     return (
-      <div id='createExperiencePage'>
-        <Header noSearch title='New Experience' />
+      <div id='experienceFormPage'>
+        <Header noSearch title={this.props.title} />
         <ExperienceForm />
         <Footer />
       </div>
@@ -16,4 +16,4 @@ class CreateExperiencePage extends Component {
   }
 }
 
-export default CreateExperiencePage;
+export default ExperienceFormPage;
