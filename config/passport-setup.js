840f8bcf9b2562dbdad85f05c7ed341b54e893d5
Hapachino <erickwbrownfield@gmail.com>
2019-01-20 13:34:00 -0800

    oauth: check if users exists, and create if not

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,36 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const config = require('./oauth');
+const db = require('../db');
+const mysql = require('mysql');
 
 const configuredPassport = passport.use(
-  new GoogleStrategy(config, (accessToken, refreshToken, profile, done) => {
-    console.log('profile: ', profile);
+  new GoogleStrategy(config, async (accessToken, refreshToken, profile, done) => {
+    const { id: google_id, name: { familyName: lastname, givenName: firstname} } = profile;
+
+    try {
+      let prepared = `SELECT * 
+                      FROM users AS u
+                      WHERE u.google_id = ?`;
+      let inserts = [google_id];
+      let query = mysql.format(prepared, inserts);
+      const [user] = await db.query(query);
+
+      if (user) {
+        console.log(user);
+      } else {
+        prepared = `INSERT INTO users (google_id, firstname, lastname)
+                    VALUES (?, ?, ?)`;
+        inserts = [google_id, firstname, lastname];
+        query = mysql.format(prepared, inserts);
+        await db.query(query);
+      }
+
+    } catch (err) {
+      console.log('Error getting/creating user', err);
+    }
+
+    console.log('profile: ', profile.id);
     console.log('access token: ', accessToken);
     return done(null, profile);
   }
