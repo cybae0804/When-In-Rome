f60065ca7500deecf4adf30e1711d75ca6ec1811
Cy Bae <cybae0804@gmail.com>
2019-01-23 10:36:38 -0800

    Basic structure for dashboard

--- client/src/components/dashboard_page/dashboard/dashboard.js
+++ client/src/components/dashboard_page/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUser } from '../../../actions';
+import axios from 'axios';
 
 // Components
 import Upcoming from '../upcoming/upcoming';
@@ -11,13 +12,35 @@ import History from '../history/history';
 import './dashboard.css'
 
 class Dashboard extends Component {
+  state = {
+    asUser: true
+  }
+
+  async componentDidMount() {
+    // MAKE THE AXIOS CALL HERE AND SET THE STATE APPROPRIATELY
+    const data = await axios.get('/api/');
+  }
+
+  toggleStatus = () => {
+    this.setState({
+      asUser: !this.state.asUser
+    });
+  }
 
   render() {
+    // WITH DATA FROM COMPONENTDIDMOUNT, SET VARIABLES PROPERLY AND
+    // PASS IT DOWN TO COMPONENTS
+    
+    const upcomingData = this.state.asUser ? 'hello' : 'bye';
+    const reservationsData = this.state.asUser ? 'hello' : 'bye';
+    const activeListingData = this.state.asUser ? 'hello' : 'bye';
+    const historyData = this.state.asUser ? 'hello' : 'bye';
+
     return (
       <div>
         <div className="ui equal width grid container topMargin">
-          <button className="ui column button"><h3>As Host</h3></button>
-          <button className="ui column button"><h3>As User</h3></button>
+          <button onClick={this.toggleStatus} className={`ui column button ${this.state.asUser ? '' : 'positive'}`}><h3>As Host</h3></button>
+          <button onClick={this.toggleStatus} className={`ui column button ${this.state.asUser ? 'positive' : ''}`}><h3>As User</h3></button>
         </div>
         <Upcoming />
         <Reservations />
