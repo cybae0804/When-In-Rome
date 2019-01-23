3ea52030028d8c84d292c387868d60863de33c0b
Hapachino <erickwbrownfield@gmail.com>
2019-01-23 10:47:28 -0800

    created host dashboard route

--- routes/api/booked.js
+++
@@ -1,7 +0,0 @@
-const bookedRouter = require('express').Router();
-const { booked } = require('../../controllers');
-
-bookedRouter.route('/')
-  .get(booked.getHostDashboard);
-
-module.exports = bookedRouter;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.use('/experiences', require('./experiences'));
-router.use('/booked', require('./booked'));
+router.use('/dashboard', require('./dashboard'));
 router.use('/', require('./images'));
 
 module.exports = router;
