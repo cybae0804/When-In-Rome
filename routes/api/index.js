021983262a88d0a144f3c9b8bceb5b8a12e5c7e6
Hapachino <erickwbrownfield@gmail.com>
2019-01-24 18:56:03 -0800

    fixed post to dates

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.use('/experiences', require('./experiences'));
 router.use('/dashboard', require('./dashboard'));
+router.use('/dates', require('./dates'));
 router.use('/', require('./images'));
 
 module.exports = router;
