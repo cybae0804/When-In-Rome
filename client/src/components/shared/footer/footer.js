e0d737d94a41e8d38a959cac440c5f56c2d64de6
Cy Bae <cybae0804@gmail.com>
2019-01-30 23:46:57 -0800

    footer revised and small bug fixes

--- client/src/components/shared/footer/footer.js
+++ client/src/components/shared/footer/footer.js
@@ -5,7 +5,7 @@ export default props => (
   <div id='footer'>
     <div className="footerSpace"></div>
     <div className="mainContent">
-      <div>About Us</div>
+      <div>When In Rome ©</div>
     </div>
   </div>
 );
