712408de9bd44378e64e56d840abeec9a8c1c997
Hapachino <erickwbrownfield@gmail.com>
2019-02-01 13:34:11 -0800

    leave review now redirect backs to page after log in

--- routes/api/dates.js
+++ routes/api/dates.js
@@ -4,6 +4,8 @@ const { dates } = require('../../controllers');
 datesRouter.route('/')
   .get(dates.get)
   .post(dates.post)
+
+datesRouter.route('/book')
   .post(dates.postOne);
 
 module.exports = datesRouter;
